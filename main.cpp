b5a2a79cebe3767e2f82a86a20b7b5a6658e2cef
Dmitry Ermachenkov <korvin14@gmail.com>
2016-03-22 00:02:05 +0300

    initial file

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+ * algo_tp_2/main.cpp
+ *
+ *   Created on: Mar 22, 2016
+ *       Author: Dmitry Ermachenkov
+ *      License: free-to-use if you refernce me
+ *  Description:
+ */
+
+
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(void) {
+  cout << "Hello bitches" << endl;
+  return 0;
+}
